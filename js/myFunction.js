b4d4fdf30e7258812f9dd0a159c969de1352166b
Harshit kumar <106154167+harshitkumar2003@users.noreply.github.com>
2024-05-31 02:30:21 +0530

    restore the old code and delete test code

--- js/myFunction.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-    x.classList.toggle("change");
-
-}
--- js/toggle.js
+++
@@ -1,6 +0,0 @@
-const toggler = document.querySelector('.navbar-toggler');
-const collapse = document.querySelector('.collapse');
-
-toggler.addEventListener('click', () => {
-  collapse.classList.toggle('show');
-});
