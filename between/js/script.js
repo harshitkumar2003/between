dfd577e193ea8225a48e779d18b160901e40ed24
Harshit Kumar <harshitkumar2k03@gmail.com>
2024-08-07 22:46:41 +0530

    "Added CSS styles and JavaScript code for blog page layout and interactive effects"

--- between/js/script.js
+++ between/js/script.js
@@ -268,3 +268,34 @@ jQuery(document).ready(function($){
     });
   }
 });
+
+
+// for blog page
+let articles = document.querySelectorAll(".article");
+
+articles.forEach(i => {
+  i.addEventListener(
+    "mousemove",
+    e => {
+      let mouseX = e.offsetX;
+      let mouseY = e.offsetY;
+      i.querySelector(".overlay")
+        .style.setProperty(
+        "background-image",
+        `radial-gradient(circle at ${(mouseX) * 100  / -i.offsetWidth+100}% ${(mouseY) * 100  / -i.offsetHeight+100}%,rgba(0,0,0,0.2) 25%,rgba(0,0,0,0.33) 50%)`
+      );
+      i.style.setProperty("transform", `rotateY(${  ( ( (mouseX*100) / i.offsetWidth - 50 ) / 100) * 3}deg) rotateX(${  ( ( (mouseY*100) / i.offsetHeight - 50 ) / 100) * 3}deg) `
+)
+    },
+    false
+  );
+  i.addEventListener("mouseleave",()=>{
+    i.style.setProperty("transform",`rotateX(0deg) rotateY(0deg)`);
+    
+          i.querySelector(".overlay")
+        .style.setProperty(
+        "background-image",
+        `radial-gradient(circle at 50% 50%,rgba(0,0,0,0.2) 20%,rgba(0,0,0,0.3) 50%)`
+      );
+  })
+});
